79e5dd5b6fc1ce8a23c45b8ed8bfb63fb2ca14ee
wafewu <148876122+wafewu@users.noreply.github.com>
2024-10-04 10:45:27 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -76,6 +76,9 @@ git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 # MosDNS
 #git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
 # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
